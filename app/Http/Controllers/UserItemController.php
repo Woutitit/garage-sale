6a6b0b2ada50719491d0e096c7733928fdf4a86c
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-23 22:18:49 +0200

    Add basic data lookup for user items

--- app/Http/Controllers/UserItemController.php
+++ app/Http/Controllers/UserItemController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
 
 class UserItemController extends Controller {
 
     public function index() {
+
+    	Item::where('user_id', Auth::id())->get();
     	return view('pages.user.items');
     }
 }
--- resources/views/pages/user/items.blade.php
+++ resources/views/pages/user/items.blade.php
@@ -2,11 +2,73 @@
 
 @section('content')
 <div class="container">
-@if(Auth::check())
-<h1>My Items</h1>
-<a href="/items/create" class="btn btn-success">Create item</a>
-@else
-<h1>Items of Wout Borghgraef</h1>
-@endif
+	@if(Auth::check())
+	<h1>My Items</h1>
+	<a href="/items/create" class="btn btn-success">Create item</a>
+	@else
+	<h1>Items of Wout Borghgraef</h1>
+	@endif
+	<div class="row">
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<img src="..." alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
