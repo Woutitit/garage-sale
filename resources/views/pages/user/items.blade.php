da1a6d9547b057a06b3a575d5aff6543857c49db
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-23 23:22:37 +0200

    Add button links to show and edit user item

--- resources/views/pages/user/items.blade.php
+++ resources/views/pages/user/items.blade.php
@@ -22,7 +22,7 @@
 				<div class="caption">
 					<h3>{{ $item->name }}</h3>
 					<p>{{ $item->description }}</p>
-					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+					<p><a href="{{ '/items/' . $item->id }}" class="btn btn-primary" role="button">Bekijken</a> <a href="{{ '/items/' . $item->id . '/edit' }}" class="btn btn-default" role="button">Bewerken</a></p>
 				</div>
 			</div>
 		</div>
