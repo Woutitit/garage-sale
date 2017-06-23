c43c01f7797f6e968ef2417399c2941e4bdd67f6
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-23 17:44:49 +0200

    add category seeding functionality

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -4,15 +4,20 @@ use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder {
 
-	$category_names = [
+	public $category_names = [
 	'Strand'
-	]
+	];
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run() {
-        //
+    	foreach($this->category_names as $cName) {
+    		DB::table('categories')->insert([
+    			'name' => $cName,
+    			]);
+    	}
     }
 }
