e782e0cd185b668aa569eab0092226efbc00a3a3
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-06-24 04:13:43 +0200

    Change lookup items id to general id so everyone sees the same

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use Auth;
 
 class UserController extends Controller {
     public function showItems($user_path) {
-    	$items = Item::where('user_id', Auth::id())->get();
+    	$items = Item::where('user_id', $this->getUserIdByUserPath($user_path))->get();
 
     	return view('pages.user.items', ['items' => $items, 'user_name' => $this->getUserNameByUserPath($user_path)]);
     }
@@ -20,5 +20,9 @@ class UserController extends Controller {
 
     public function getUserNameByUserPath($user_path) {
     	return User::where('path', $user_path)->value('name');
+    }
+
+    public function getUserIdByUserPath($user_path) {
+    	return User::where('path', $user_path)->value('id');
     }
 }
